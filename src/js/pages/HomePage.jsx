d11f6e6b1b2551e7b9dd2ec79c555c9363b42c79
tvanro <tom@tvanro.be>
2017-05-14 14:18:55 +0800

    add link to blog post

--- src/js/pages/HomePage.jsx
+++ src/js/pages/HomePage.jsx
@@ -10,7 +10,7 @@ export default class HomePage extends Component {
           <h1><TranslationContainer translationKey="title"/></h1>
           <p>This is a simple example project to demonstrate how to build a translation component with React and Redux.</p>
           <p>Source code is available here: <a href="https://github.com/tvanro/react-redux-translation-example" target="_blank">https://github.com/tvanro/react-redux-translation-example</a></p>
-          <p>Blog post will follow soon!</p>
+          <p>Read the <a href="http://www.tvanro.be/tech/building-a-translation-component-with-react-and-redux/" target="_blank">blog post</a> for more details!</p>
         </div>
         <div className="col-sm-6">
           <table>
