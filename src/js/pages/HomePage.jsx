5fe215b4273a20bd3189615c59c92de2a60a8bd1
tvanro <tom@tvanro.be>
2017-05-26 22:42:43 +0800

    improve responsiveness

--- src/js/pages/HomePage.jsx
+++ src/js/pages/HomePage.jsx
@@ -6,13 +6,13 @@ export default class HomePage extends Component {
   render() {
     return (
       <div className="row">
-        <div className="col-sm-6">
+        <div className="col-md-6">
           <h1><TranslationContainer translationKey="title"/></h1>
           <p>This is a simple example project to demonstrate how to build a translation component with React and Redux.</p>
           <p>Source code is available here: <a href="https://github.com/tvanro/react-redux-translation-example" target="_blank">https://github.com/tvanro/react-redux-translation-example</a></p>
           <p>Read the <a href="http://www.tvanro.be/tech/building-a-translation-component-with-react-and-redux/" target="_blank">blog post</a> for more details!</p>
         </div>
-        <div className="col-sm-6">
+        <div className="col-md-6">
           <table>
             <caption><TranslationContainer translationKey="table_caption"/></caption>
             <thead>
